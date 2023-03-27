dc22d0d3f01d10a0cb80efcc10551ecfcf25cf62
Hazem Shehawy <hazemk537@gmail.com>
2023-03-27 18:04:31 +0200

    init 00

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Nav from './components/Nav'
 import './App.css'//why
+import Hero from './components/Hero'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-     
-     
+      <Nav/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+
+
+export default function Hero()
+{
+    return (
+<div className="hero section">
+        <img className="hero--image"src="public/hero-section.jpg" alt="plane image"/>
+        <h1>Online Experiences</h1>
+        <p>Join unique interactive activities led by one-of-a-kind
+        hosts—all without leaving home.</p>
+        </div>
+    )
+}
